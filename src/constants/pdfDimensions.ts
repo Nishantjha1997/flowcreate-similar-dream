91a2d38cc405b10690cb8273e612e4c81317e91b
Nishantjha1997 <nishantjha31@gmai.com>
2026-07-16 21:22:01 +0530

    [P10-T2] Introduce A4 dimension constants

---
+++ src/constants/pdfDimensions.ts
@@ -0,0 +1,8 @@
+/** A4 width at 96dpi (210mm) */
+export const A4_WIDTH_PX = 794;
+/** A4 height at 96dpi (297mm) */
+export const A4_HEIGHT_PX = 1123;
+/** A4 width in mm */
+export const A4_WIDTH_MM = 210;
+/** A4 height in mm */
+export const A4_HEIGHT_MM = 297;
